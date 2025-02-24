7bb8833da2439e95a699039e7f2e155b1818d50b
fabianszyman <fabianszyman@gmail.com>
2025-02-24 21:11:37 +0100

    remove useState

--- components/UserSection.tsx
+++ components/UserSection.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import Auth from '@/components/Auth';
 import { Session } from '@/types/supabase';
@@ -9,11 +8,9 @@ import { Session } from '@/types/supabase';
 const Profile = dynamic(() => import('@/components/Profile'), { ssr: false });
 
 export default function UserSection({ initialSession }: { initialSession: Session | null }) {
-  const session = initialSession;
-
   return (
     <div className="mt-10">
-      {session ? <Profile /> : <Auth />}
+      {initialSession ? <Profile /> : <Auth />}
     </div>
   );
 } 
