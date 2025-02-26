b7442e22fd854cdf5624c5129d2c4736160f9d6a
fabianszyman <fabianszyman@gmail.com>
2025-02-26 09:01:12 +0100

    Refactor ThemeProvider with explicit TypeScript interface

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { ReactNode } from "react";
+
+interface ThemeProviderProps {
+  children: ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+  storageKey?: string;
+  themes?: string[];
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
